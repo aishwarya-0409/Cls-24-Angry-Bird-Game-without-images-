ef63a7e6456579d0b3dd7bdc2c69ea67a9b03d7c
aishwarya-0409 <73163380+aishwarya-0409@users.noreply.github.com>
2020-11-26 18:08:05 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,27 @@
+class Bird {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.5,
+          'friction':1.0,
+          'density':1.5
+      }
+      this.body = Bodies.circle(x, y, 30, options);
+     
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('pink')
+      fill("red");
+      ellipse(0, 0, 30);
+      pop();
+    }
+  };
+  
